c7f2022c1f10faa0781dbe3b4872810cfe42e262
rmnsfx <rmnsfx@users.noreply.github.com>
2016-11-24 13:01:09 +0500

    Update wsgi.py

--- hello/hello/wsgi.py
+++ hello/hello/wsgi.py
@@ -8,10 +8,13 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.stdout = sys.stderr
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hello.settings_heroku")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hello.settings")
 
 #application = get_wsgi_application()
 
